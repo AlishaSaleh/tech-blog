1ebf7c53ada6cea2b04c0390611f839cee197779
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-27 16:54:09 +0000

    Comment associations added

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
+// User and Blog associations - users have many blogs - a blog belongs to a user
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -10,4 +12,22 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+// Comment to User associations - users have many comments - a comment belongs to a user
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Comment to Blog associations - blogs have many comments - a comment belongs to a blog
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
